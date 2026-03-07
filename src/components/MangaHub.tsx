aa37b7317ae238c9ee91cf2e7c5f6d5a8984b86c
MKPlaza <samuelbryant144@gmail.com>
2026-03-07 15:33:23 -0600

    Update MangaHub.tsx

--- src/components/MangaHub.tsx
+++ src/components/MangaHub.tsx
@@ -17,7 +17,7 @@ export default function MangaHub({ favorites, onToggleFavorite }: MangaHubProps)
           MK-PLAZA MANGA
         </h1>
         <p className="text-[10px] text-gray-400 mt-1 uppercase tracking-widest">
-          Aetherium Archive v2.0
+          For those who like to read rather than watch
         </p>
       </div>
 
